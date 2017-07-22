9302ce1da46de9757caa76e7758da5772f88eef2
Russell Baldwin <russ@shoesforindustry.net>
2017-07-22 10:12:23 +0100

    The inevitable typo fixed

--- uninextprev.php
+++ uninextprev.php
@@ -1,32 +1,32 @@
 <?php
-/**  
+/**
  * uniNextPrev plugin
  *
- * It provide variables with links and page titles for next/prev pages and can move through the site, 
- * up and down, through parent/children/grandchildren. 
+ * It provide variables with links and page titles for next/prev pages and can move through the site,
+ * up and down, through parent/children/grandchildren.
  * Also seamlessly moves through pagination (e.g. articles index) if you set it.
  * So a sort of universal next / prev, hence 'uniNextPrev'
- * 
+ *
  * WARNING: ONLY works at parent/child/grandchild menu levels - fails if you go any deeper!
  * Four levels is probably too deep for a menu anyway - but you have been warned.
- *   
- * WARNING: You must set a comma seperated quoted list of page uid's 
+ *
+ * WARNING: You must set a comma seperated quoted list of page uid's
  * for which you want to ignore the children of in next/prev, in your config.php
  *  e.g.
  *    c::set('uniNextPrev_ignore_children',array('press','articles','about'));
- *  It can be empty  
+ *  It can be empty
  *    c::set('uniNextPrev_ignore_children',array(''));
- * 
+ *
  * Basic call uniNextPrev plugin in yoru template:
- *  $uniNextPrev = uninextprev(); 
- * 
+ *  $uniNextPrev = uninextprev();
+ *
  * Call uniNextPrev plugin with pagination (call after setting pagination!)
- *  $uniNextPrev = uninextprev(array('pagination' => $articles->pagination())); 
+ *  $uniNextPrev = uninextprev(array('pagination' => $articles->pagination()));
  *
  * See README for much more information!
- * 
+ *
  * @author Russ Baldwin
- * @version v0.1.5
+ * @version v0.1.6
  * @copyright shoesforindustry.net, 06 Nov, 2014
  * @package Kirby Plugin
 **/
@@ -37,29 +37,29 @@ function uninextprev($options = array()) {
   $defaults = array(
     'pagination' => ''
   );
-  
+
   // variables
   $nextPageURL='';
   $nextPageTitle='';
   $prevPageURL='';
   $prevPageTitle='';
-  
+
   // merge defaults and options
   $options = array_merge($defaults, $options);
   $pagination = $options['pagination'];
   //uniNextPrev_ignore_children list is pulled from config.php so set it!
-  $ignoreChildren = c::get('uniNextPrev_ignore_children'),array());
-   
+  $ignoreChildren = c::get('uniNextPrev_ignore_children',array());
+
   // ***************************************************
 
   // Next
-  
-  // We have pagination set when calling the plugin 
+
+  // We have pagination set when calling the plugin
   if($pagination AND $pagination->hasNextPage()){
    $nextPageURL=$pagination->nextPageURL();
    $nextPageTitle=page()->title().': '.$pagination->nextPage();
   }
-  // We have visible children so jump down into them 
+  // We have visible children so jump down into them
   elseif (page()->hasVisibleChildren() AND !in_array(page()->uid(), $ignoreChildren) ) {
    $nextPageURL=page()->children()->first()->url();
    $nextPageTitle=page()->children()->first()->title();
@@ -87,7 +87,7 @@ function uninextprev($options = array()) {
 // Previous
 if (!page()->isHomePage()) {
 
-// We have pagination set when calling the plugin  
+// We have pagination set when calling the plugin
 if ($pagination AND $pagination->hasPrevPage() ){
   $prevPageURL=$pagination->prevPageURL();
   $prevPageTitle=page()->title().': '.$pagination->prevPage();
@@ -117,7 +117,7 @@ elseif (page()->prevVisible()->hasVisibleChildren() AND !in_array(page()->prevVi
     $prevPageURL=page()->PrevVisible()->children()->last()->url();
     $prevPageTitle=page()->PrevVisible()->children()->last()->title();
   }
-} 
+}
 
 elseif (page()->prevVisible()) {
   // Jump to previous page
@@ -126,12 +126,12 @@ elseif (page()->prevVisible()) {
 }
 
 }
-  
+
   $results=array(
     "nextPageURL"=>$nextPageURL,
     "nextPageTitle"=>$nextPageTitle,
     "prevPageURL"=>$prevPageURL,
-    "prevPageTitle"=>$prevPageTitle 
+    "prevPageTitle"=>$prevPageTitle
   );
 
   return $results;
